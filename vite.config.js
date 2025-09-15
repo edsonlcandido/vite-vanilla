2c7902d796aa5cbb86a8b799aef6cfab2b1f5ed8
Edson Candido <edson.candido@chartindustries.com>
2025-09-15 15:30:33 -0300

    feat: initialize Vite project with Tailwind CSS and basic setup
    - Added package.json for project configuration and dependencies
    - Included Vite logo SVG and JavaScript logo SVG
    - Created counter.js for counter functionality
    - Developed main.js to set up the application structure
    - Added style.css for basic styling and responsive design
    - Configured Vite with Tailwind CSS in vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\CandidED\OneDrive - Chart Industries Inc\Visual Studio Projects\edsonlcandido\vite-vanilla\vite.config.js
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
