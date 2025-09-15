188ee24a8f52d48a201bee5e0ca54c35bc2df85e
Edson Candido <edson.candido@chartindustries.com>
2025-09-15 16:28:17 -0300

    Remove Tailwind CSS and related files, delete counter functionality, and clean up main application structure

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 // filepath: c:\Users\CandidED\OneDrive - Chart Industries Inc\Visual Studio Projects\edsonlcandido\vite-vanilla\vite.config.js
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    //tailwindcss(),
   ],
 })
